c621b4d12b6e4fa248db9ab9fb9f4461f3341816
IvaloWogi <ivaylo@wogi.biz>
2021-10-26 16:34:24 +0300

    scss installed

---
+++ components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <b-button class="button" @click="clickMe">Click Me</b-button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Button',
+    methods: {
+      clickMe() {
+          this.$buefy.notification.open('Clicked!!')
+      }
+    }
+  }
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+
+    <div class="container column is-10">
+      <Nuxt />
+    </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
